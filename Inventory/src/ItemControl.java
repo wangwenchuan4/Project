52e8c443f4ed53b211236b22ac8c4d5593fa5656
ggertsen <grant-gertsen@uiowa.edu>
2016-11-07 17:13:18 -0600

    Formed the basis of ItemControl

--- Inventory/src/ItemControl.java
+++ Inventory/src/ItemControl.java
@@ -1,5 +1,13 @@
+import java.util.*;
 
 public class ItemControl {
 
+	ItemList items = new ItemList();
+	
+	/**
+	 * TODO: From a csv which I have, read in the items.
+	 */
+	public ItemControl() {
+	}
 }
                           
