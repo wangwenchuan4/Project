20e8e96f1716a17f89542b43f720e3d9290036a6
ggertsen <grant-gertsen@uiowa.edu>
2016-11-07 22:36:57 -0600

    Added some more clarification on ItemControl

--- Inventory/src/ItemControl.java
+++ Inventory/src/ItemControl.java
@@ -6,6 +6,7 @@ public class ItemControl {
 	
 	/**
 	 * TODO: From a csv which I have, read in the items.
+	 * Will be similar to the "Birds" Maven project in terms of reading in data (initially)
 	 */
 	public ItemControl() {
 	}
