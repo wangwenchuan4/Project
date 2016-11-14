e9de1c66b419ed62675317710ea50ae215d6a43e
ggertsen <grant-gertsen@uiowa.edu>
2016-11-13 19:40:36 -0600

    Moving files to the new directory

---
+++ project-cs2820/src/warehouse/Stock.java
@@ -0,0 +1,43 @@
+package warehouse;
+
+import java.util.*;
+
+public class Stock {
+
+	ArrayList<Item> itemList;
+	
+	public Stock() {
+		itemList = new ArrayList<Item>();
+	}
+	
+	public void addItem(Item item) {
+		itemList.add(item);
+	}
+	
+	public int numberInStock(String itemName) {
+		int stock = 0;
+		for(Item item : itemList) {
+			if(item.getItemName().equals(itemName)) {
+				stock++;
+			}
+		}
+		return stock;
+	}
+	
+	public int numberInStock(int serialNumber) {
+		int stock = 0;
+		for(Item item : itemList) {
+			System.out.println("Serial number is " + item.getItemName());
+			if(item.getSerialNumber() == serialNumber) {
+				stock++;
+			}
+		}
+		return stock;
+	}
+	
+	public Item getItemAtIndex(int index) {
+		Item item = itemList.get(index);
+		return item;
+	}
+	
+}
