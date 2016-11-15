aae505a89f74022e93663b8101328849a804b108
Peter Nelson <peter_nelson@outlook.com>
2016-11-14 20:39:40 -0600

    adding Belt files to new project. Starting to implement Tickable in Belt class

---
+++ project-cs2820/src/warehouse/Belt.java
@@ -0,0 +1,48 @@
+
+package warehouse;
+
+import java.util.ArrayList;
+
+
+public class Belt implements Tickable {
+	
+	//double speed;
+	double beltWidth;
+	int beltCapacity;  //determined by belt length -- does floor determine this?
+	ArrayList<Bin> currentBins = new ArrayList<Bin>();
+	
+	String beltID;
+	
+	public Belt(String beltID, int beltCapacity, double beltWidth) {
+		this.beltID = beltID;
+		this.beltCapacity = beltCapacity;
+		this.beltWidth = beltWidth;
+	}
+
+	public void pauseBelt() {
+		// pause belt
+	}
+	
+	public void resumeBelt() {
+		//resume
+	}
+	
+	public ArrayList<Bin> getCurrentBins() {
+		return currentBins;
+	}
+	
+	public void addBin(Bin binId) {
+		//add bin
+	}
+	
+	public void remove(Bin binId) {
+		//remove bin
+	}
+
+	@Override
+	public void tick(int count) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ project-cs2820/src/warehouse/Bin.java
@@ -0,0 +1,21 @@
+package warehouse;
+
+
+public class Bin {
+	
+	  Order order;  // ready to go on the Belt
+	  boolean finished;  // initially false, becomes true when ready to go
+	  public Bin() {
+		  order = null;
+		  finished = false;
+	  }
+	  
+	  public boolean isFinished() { return finished; }
+	  
+	  public void setFinished() { finished = true; }
+	  
+	  public Order getOrder() { return order; }
+	  
+	  public void setOrder(Order o) { order = o; }
+	  
+	}
