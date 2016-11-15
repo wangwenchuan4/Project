fe3341c587e487228eb767564bc07fe23d19004b
Peter Nelson <peter_nelson@outlook.com>
2016-11-14 21:31:48 -0600

    more javadoc stuff.  Method renaming

--- project-cs2820/src/warehouse/BeltTester.java
+++ project-cs2820/src/warehouse/BeltTester.java
@@ -6,21 +6,22 @@ import org.junit.Test;
 
 public class BeltTester {
 	
-	/*# Class for testing functionality of Belt and Bin classes 
+	/* Class for testing functionality of Belt and Bin classes 
 	 * 
 	 * */
 	
-	
 	@Test
-	public void testBelt() {
+	public void testBeltCapacity() {
 		Order order = null;
 		Belt beltTester = new Belt("belt1", 2, 2);
 		Bin bin = new Bin();
 		bin.setOrder(order);
 		beltTester.addBin(bin);
-		
+		assertEquals(1, beltTester.getNumberOfBins());
 		
 	}
+	
+	
 	
 
 	
--- project-cs2820/src/warehouse/Bin.java
+++ project-cs2820/src/warehouse/Bin.java
@@ -3,6 +3,10 @@ package warehouse;
 
 public class Bin {
 	
+	/**
+	 * Borrowed from Prof. Herman -- May chanage implementation as needed.
+	 * More of a placeholder for the moment.
+	 */
 	  Order order;  // ready to go on the Belt
 	  boolean finished;  // initially false, becomes true when ready to go
 	  public Bin() {
